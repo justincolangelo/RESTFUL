4f00be3c3f4a8785c9799d29219def576e373d5c
Justin Colangelo <justincolangelo@gmail.com>
2023-08-27 17:59:56 -0400

    add async

---
+++ Interfaces/IItemsRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RESTFUL.Entities;
+
+namespace RESTFUL.Interfaces
+{
+    public interface IItemsRepository
+    {
+        public Task<IEnumerable<Item>> GetItemsAsync();
+
+        public Task<Item> GetItemAsync(Guid id);
+        Task CreateItemAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(Guid id);
+    }
+}
