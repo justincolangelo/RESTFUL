ed72389d02597f61deaa395a7b6f61ea90bb4cb1
Justin Colangelo <justincolangelo@gmail.com>
2023-08-27 17:08:51 -0400

    add mongodb provider and hook it up to repository

---
+++ Settings/MongoDBSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTFUL.Settings
+{
+    public class MongoDBSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string ConnectionString
+        {
+            get
+            {
+                return $"mongodb://{Host}:{Port}";
+            }
+        }
+    }
+}
