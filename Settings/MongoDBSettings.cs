96c8cc6f131c5d7443eea672a19a5315a23805b2
Justin Colangelo <justincolangelo@gmail.com>
2023-08-27 20:28:58 -0400

    add example of using secrets for password into db

--- Settings/MongoDBSettings.cs
+++ Settings/MongoDBSettings.cs
@@ -9,11 +9,18 @@ namespace RESTFUL.Settings
     {
         public string Host { get; set; }
         public int Port { get; set; }
+        public string User { get; set; }
+        // populated at runtime by secret manager
+        public string Password { get; set; }
         public string ConnectionString
         {
             get
             {
                 return $"mongodb://{Host}:{Port}";
+                // if you've created a secret for password you can use this connection string
+                // dotnet user-secrets init
+                // dotnet user-secrets set MongoDBSettings:Password somepassword
+                //return $"mongodb://{User}:{Password}@{Host}:{Port}";
             }
         }
     }
